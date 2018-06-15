3a7cbeadf9f5e1c695072bdf6a63fb191e7576b4
Stolbikova <alina.stolbikova@gmail.com>
2018-06-15 16:26:39 +0300

    Added functions

--- src/todolist.js
+++ src/todolist.js
@@ -1,13 +1,41 @@
-function addTodo() {
-  var input = document.getElementById('input');
-  var t = document.createTextNode(input.value);
-  var ul = document.getElementById("list");
-  var li = document.createElement("li");
-  li.appendChild(t);
-  ul.appendChild(li);
-}
+window.addEventListener("load", function() {
 
-window.onload = function() {
-  var btn = document.getElementById('addBtn');
-  btn.onclick() = addTodo;
-}
+  var ul = document.getElementById("list");
+
+  function addItem() {
+
+    let input = document.getElementById('input');
+    if (input.value === '') {
+      alert("You must write something!");
+      return;
+    }
+    let t = document.createTextNode(input.value);
+    let li = document.createElement("li");
+    li.appendChild(t);
+    ul.appendChild(li);
+  }
+
+  function removeItem() {
+
+    // ul.removeChild(ul.childNodes[i]);
+  }
+
+  var addbtn = document.getElementById('addBtn');
+  addbtn.addEventListener("click", function() {
+    addItem();
+  });
+
+  var rmvbtn = document.getElementById('removeBtn');
+  addbtn.addEventListener("click", function() {
+    removeItem();
+  });
+
+  var list = document.getElementsByTagName("li");
+
+  for (let i = 0; i < list.length; i++) {
+
+    list[i].onclick =  function() {
+      list[i].classList.add("checked");
+    }
+  }
+});
